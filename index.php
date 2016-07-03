a9461d8ee00e2ec08815ba35ae7f64a4e21281e0
huydoan95 <huydoan95.httt@gmail.com>
2016-07-03 18:48:13 +0700

    alpha version

---
+++ index.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huydoan
+ * Date: 6/28/2016
+ * Time: 7:17 PM
+ */
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="Đoàn Hoàng Huy">
+
+    <title>Sóng Biển Đông Tourist</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Customizable CSS -->
+    <link href="assets/css/main.css" rel="stylesheet" data-skrollr-stylesheet>
+    <link href="assets/css/green.css" rel="stylesheet" title="Color">
+    <link href="assets/css/owl.carousel.css" rel="stylesheet">
+    <link href="assets/css/owl.transitions.css" rel="stylesheet">
+    <link href="assets/css/animate.min.css" rel="stylesheet">
+    <link href="assets/css/elasticstroke.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,900,300,700" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic" rel="stylesheet">
+
+    <!-- Icons/Glyphs -->
+    <link href="assets/fonts/fontello.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <!--<link rel="shortcut icon" href="assets/images/favicon.ico">-->
+
+    <!-- HTML5 elements and media queries Support for IE8 : HTML5 shim and Respond.js -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!-- ============================================================= HEADER ============================================================= -->
+
+<header class="dark-bg">
+        <div class="container">
+            <!-- ============================================================= LOGO ============================================================= -->
+            <div class="flex-container">
+                <div class="flex-item item1 text-right">
+                    <a href="index.php">
+                        <img src="images/logo.png" class="logo" alt="">
+                    </a>
+                </div>
+                <div class="flex-item item2 text-center">
+                        <!--<h1>Sóng Biển Đông Tourist</h1>-->
+                        <h1>
+                            <svg viewBox="0 0 auto auto">
+                                <symbol id="s-text">
+                                    <text text-anchor="middle"
+                                          x="50%"
+                                          y="50%"
+                                          class="text--line2"
+                                    >
+                                        Sóng Biển Đông
+                                    </text>
+                                    <text text-anchor="middle"
+                                          x="50%"
+                                          y="83%"
+                                          class="text--line2"
+                                    >
+                                        Tourist
+                                    </text>
+
+                                </symbol>
+
+                                <g class="g-ants">
+                                    <use xlink:href="#s-text"
+                                         class="text-copy"></use>
+                                    <use xlink:href="#s-text"
+                                         class="text-copy"></use>
+                                    <use xlink:href="#s-text"
+                                         class="text-copy"></use>
+                                    <use xlink:href="#s-text"
+                                         class="text-copy"></use>
+                                    <use xlink:href="#s-text"
+                                         class="text-copy"></use>
+                                </g>
+
+
+                            </svg>
+
+                        </h1>
+                </div>
+            </div>
+
+
+            <!-- ============================================================= LOGO : END ============================================================= -->
+
+        </div><!-- /.container -->
+</header>
+
+<!-- ============================================================= HEADER : END ============================================================= -->
+
+
+<!-- ============================================================= MAIN ============================================================= -->
+
+<main>
+
+    <!-- ============================================================= SECTION – HERO ============================================================= -->
+
+    <section id="hero">
+        <div id="owl-main" class="owl-carousel height-lg owl-inner-nav owl-ui-lg">
+
+            <div class="item">
+                <div class="container">
+                    <img class="" src="images/binhba.png" width="720" height="480">
+                </div><!-- /.container -->
+            </div><!-- /.item -->
+
+            <div class="item">
+                <div class="container">
+                    <img class="" src="images/dalat.png" width="720" height="480">
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="container">
+                    <img class="" src="images/dongthap-angiang.png" width="720" height="480">
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="container">
+                    <img class="" src="images/namdu-balua.png" width="720" height="480">
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="container">
+                    <img class="" src="images/tiengiang.png" width="720" height="480">
+                </div>
+            </div>
+        </div><!-- /.owl-carousel -->
+    </section>
+
+    <!-- ============================================================= SECTION – HERO : END ============================================================= -->
+</main>
+
+<!-- ============================================================= MAIN : END ============================================================= -->
+
+
+<!-- ============================================================= FOOTER ============================================================= -->
+
+<footer class="dark-bg">
+    <div class="footer-bottom">
+        <div class="container inner">
+            <p class="text-center">Copyright © <?php echo date("Y"); ?> Sóng Biển Đông Tourist. All rights reserved.</p>
+        </div><!-- .container -->
+    </div><!-- .footer-bottom -->
+</footer>
+
+<!-- ============================================================= FOOTER : END ============================================================= -->
+
+<!-- JavaScripts placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.easing.1.3.min.js"></script>
+<script src="assets/js/jquery.form.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+<script src="assets/js/skrollr.min.js"></script>
+<script src="assets/js/skrollr.stylesheets.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/waypoints-sticky.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.isotope.min.js"></script>
+<script src="assets/js/jquery.easytabs.min.js"></script>
+<script src="assets/js/google.maps.api.v3.js"></script>
+<script src="assets/js/viewport-units-buggyfill.js"></script>
+<script src="assets/js/scripts.js"></script>
+</body>
+</html>
